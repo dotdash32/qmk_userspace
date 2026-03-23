200258e1609efdc197a590dcdbe7966b30766177
Josh DeWitt <josh.dewitt@skydio.com>
2026-03-23 13:08:48 -0700

    feat: add dotdash keymap

---
+++ keyboards/input_club/infinity60/rev1/keymaps/dotdash_hrm1/rules.mk
@@ -0,0 +1,19 @@
+
+# Cancel out standard features for faster bootup
+# BOOTMAGIC_ENABLE = no
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = no
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = no
+NKRO_ENABLE = no
+
+VIA_ENABLE = no
+VIAL_ENABLE = no
+
+
+# https://docs.qmk.fm/#/custom_quantum_functions?id=deferred-execution
+DEFERRED_EXEC_ENABLE = yes
+
+# https://github.com/qmk/qmk_firmware/issues/19593#issuecomment-1387476045
+# NO_USB_STARTUP_CHECK = yes
