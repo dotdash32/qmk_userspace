a4821b23c179e3ce4e039ed566c76dfcb493c7b9
Josh DeWitt <josh.dewitt@skydio.com>
2026-05-27 18:44:09 -0700

    feat: tap both LT(2, xxx) keys to toggle into SYM layer

--- keyboards/input_club/infinity60/rev1/keymaps/dotdash_hrm1/key_combos.c
+++ keyboards/input_club/infinity60/rev1/keymaps/dotdash_hrm1/key_combos.c
@@ -1,15 +1,19 @@
 enum combos {
     UI_LPRN,
     IO_RPRN,
-    OP_MINS
+    OP_MINS,
+    DK_TG2_,
 };
 
 const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM op_combo[] = {KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM dk_combo[] = {LT(2,KC_D), LT(2,KC_K), COMBO_END};
+
 
 combo_t key_combos[] = {
     [UI_LPRN] = COMBO(ui_combo, KC_LPRN),
     [IO_RPRN] = COMBO(io_combo, KC_RPRN),
     [OP_MINS] = COMBO(op_combo, KC_MINS),
+    [DK_TG2_] = COMBO(dk_combo, TG(2)),
 };
