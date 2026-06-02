1b97bc420c5fc5e4ca7b1569a20c7e0d1280db7e
Josh DeWitt <josh.dewitt@skydio.com>
2026-06-01 17:53:10 -0700

    feat: add util chords
    qw for tab and zx for grave/backtick
    
    I'm hoping this will help for weird completion stuff

--- keyboards/input_club/infinity60/keymaps/dotdash_hrm1/key_combos.c
+++ keyboards/input_club/infinity60/keymaps/dotdash_hrm1/key_combos.c
@@ -5,12 +5,16 @@ enum combos {
     IO_RPRN,
     OP_MINS,
     DK_TG2_,
+    QW_TAB_,
+    ZX_GRV_,
 };
 
 const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM op_combo[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM dk_combo[] = {LT(2,KC_D), LT(2,KC_K), COMBO_END};
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM zx_combo[] = {KC_Z, KC_X, COMBO_END};
 
 
 combo_t key_combos[] = {
@@ -18,6 +22,9 @@ combo_t key_combos[] = {
     [IO_RPRN] = COMBO(io_combo, KC_RPRN),
     [OP_MINS] = COMBO(op_combo, KC_MINS),
     [DK_TG2_] = COMBO(dk_combo, TG(2)),
+    [QW_TAB_] = COMBO(qw_combo, KC_TAB),
+    [ZX_GRV_] = COMBO(zx_combo, KC_GRV),
+};
 
 #ifdef COMMUNITY_MODULE_CUSTOM_SHIFT_KEYS_ENABLE
 #include "modules/getreuer/custom_shift_keys/custom_shift_keys.h"
