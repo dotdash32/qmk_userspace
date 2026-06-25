f603e0210a63745c400ad6f987d61bdac2b26364
Josh DeWitt <josh.dewitt@skydio.com>
2026-06-25 10:18:33 -0700

    feat: key combos, toggle num layer, tab, OSM
    try to avoid acciedentally ctr-q when alt tabbing....

--- keyboards/maple_computing/minidox/keymaps/dotdash2/key_combos.c
+++ keyboards/maple_computing/minidox/keymaps/dotdash2/key_combos.c
@@ -2,8 +2,10 @@
 
 enum combos {
     DK_TG2_,
+    L2_TG2_,
     QW_TAB_,
     WS_TAB_,
+    PUNC_TAB_, // dot-slash, aka "punctuation"
     ZX_GRV_,
     ZA_GRV_,
     CV_UNDS,
@@ -11,6 +13,7 @@ enum combos {
     UJ_LPRN,
     IK_RPRN,
     OL_MINS,
+    FJ_OSS_,
     QWET_BOOT,
     YIOP_BOOT,
 };
@@ -19,11 +22,14 @@ const uint16_t PROGMEM uj_combo[] = {KC_U, RSFT_T(KC_J), COMBO_END};
 const uint16_t PROGMEM ik_combo[] = {KC_I, LT(2,KC_K), COMBO_END};
 const uint16_t PROGMEM ol_combo[] = {KC_O, RCTL_T(KC_L), COMBO_END};
 const uint16_t PROGMEM dk_combo[] = {LT(2,KC_D), LT(2,KC_K), COMBO_END};
+const uint16_t PROGMEM L2_combo[] = {LT(2,KC_3), LT(2,KC_8), COMBO_END};  // return from L2
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM ws_combo[] = {KC_W, RCTL_T(KC_S), COMBO_END};
+const uint16_t PROGMEM punc_combo[] = {LALT_T(KC_COMM), LCTL_T(KC_DOT), COMBO_END};
 const uint16_t PROGMEM zx_combo[] = {LSFT_T(KC_Z), LCTL_T(KC_X), COMBO_END};
 const uint16_t PROGMEM zz_combo[] = {LSFT_T(KC_Z), RALT_T(KC_A), COMBO_END};
 const uint16_t PROGMEM cv_combo[] = {LALT_T(KC_C), LGUI_T(KC_V), COMBO_END};
+const uint16_t PROGMEM fj_combo[] = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
 const uint16_t PROGMEM qwet_combo[] = {KC_Q, KC_W, KC_E, KC_T, COMBO_END};
 const uint16_t PROGMEM yiop_combo[] = {KC_Y, KC_I, KC_O, KC_P, COMBO_END};
 
@@ -32,12 +38,15 @@ combo_t key_combos[] = {
     [UJ_LPRN] = COMBO(uj_combo, KC_LPRN),
     [IK_RPRN] = COMBO(ik_combo, KC_RPRN),
     [OL_MINS] = COMBO(ol_combo, KC_MINS),
-    [DK_TG2_] = COMBO(dk_combo, QK_USER_1),
+    [DK_TG2_] = COMBO(dk_combo, TG(2)),
+    [L2_TG2_] = COMBO(dk_combo, TG(2)),
     [QW_TAB_] = COMBO(qw_combo, KC_TAB),
     [WS_TAB_] = COMBO(ws_combo, KC_TAB),
+    [PUNC_TAB_] = COMBO(punc_combo, KC_TAB),
     [ZX_GRV_] = COMBO(zx_combo, KC_GRV),
     [ZA_GRV_] = COMBO(zz_combo, KC_GRV),
     [CV_UNDS] = COMBO(cv_combo, KC_UNDS),
+    [FJ_OSS_] = COMBO(fj_combo, OSM(MOD_LSFT)),
     [QWET_BOOT] = COMBO(qwet_combo, QK_BOOT),
     [YIOP_BOOT] = COMBO(yiop_combo, QK_BOOT),
 };
