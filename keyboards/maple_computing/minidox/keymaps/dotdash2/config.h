82394a265b617f382a25f476d77a80f0366446d5
Josh DeWitt <josh.dewitt@skydio.com>
2026-07-03 15:02:44 -0700

    chore: revert minidox HOOKP to perm hold
    try to have fewer 'for' -> 'Or' combos

--- keyboards/maple_computing/minidox/keymaps/dotdash2/config.h
+++ keyboards/maple_computing/minidox/keymaps/dotdash2/config.h
@@ -4,8 +4,7 @@
 
 /* QMK Home Row Mods settings */
 #define TAPPING_TERM 175
-#define PERMISSIVE_HOLD_PER_KEY
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define PERMISSIVE_HOLD
 #define FLOW_TAP_TERM 150 // aka require-prior-idle-ms
 #define FLOW_TAP_TERM_FAST 125 // for faster taps
 #define CHORDAL_HOLD
