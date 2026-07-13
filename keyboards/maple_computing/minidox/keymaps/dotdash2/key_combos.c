2eb6ae3219b43da85f1a6d551ae662249dad2de3
Josh DeWitt <dotdash32@gmail.com>
2026-07-13 03:12:52 -0700

    chore: update combos, jump to terminal, remove oldies
    hopefully fixes the ctrl/cmd + # on linux v mac in vscode

--- keyboards/maple_computing/minidox/keymaps/dotdash2/key_combos.c
+++ keyboards/maple_computing/minidox/keymaps/dotdash2/key_combos.c
@@ -5,9 +5,9 @@ enum combos {
     L2_TG2_,
     QW_TAB_,
     WS_TAB_,
-    PUNC_TAB_, // dot-slash, aka "punctuation"
+    PUNC_TAB_, // comma-period, aka "punctuation"
+    DSLH_TERM,  // dot-slash, jump to terminal
     ZX_GRV_,
-    ZA_GRV_,
     CV_UNDS,
     // replacement parens, less common bi-grams
     UJ_LPRN,
@@ -16,8 +16,9 @@ enum combos {
     FJ_OSS_,
     QWET_BOOT,
     YIOP_BOOT,
-    QET_MO3_,
-    YIP_MO3_,
+    QET_OSL3,
+    YIP_OSL3,
+    TG_WBAK,
 };
 
 const uint16_t PROGMEM uj_combo[] = {KC_U, RSFT_T(KC_J), COMBO_END};
@@ -28,14 +29,15 @@ const uint16_t PROGMEM L2_combo[] = {LT(2,KC_3), LT(2,KC_8), COMBO_END};  // ret
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM ws_combo[] = {KC_W, RCTL_T(KC_S), COMBO_END};
 const uint16_t PROGMEM punc_combo[] = {LALT_T(KC_COMM), LCTL_T(KC_DOT), COMBO_END};
+const uint16_t PROGMEM dslh_combo[] = {LCTL_T(KC_DOT), RSFT_T(KC_SLSH), COMBO_END};
 const uint16_t PROGMEM zx_combo[] = {LSFT_T(KC_Z), LCTL_T(KC_X), COMBO_END};
-const uint16_t PROGMEM zz_combo[] = {LSFT_T(KC_Z), RALT_T(KC_A), COMBO_END};
 const uint16_t PROGMEM cv_combo[] = {LALT_T(KC_C), LGUI_T(KC_V), COMBO_END};
 const uint16_t PROGMEM fj_combo[] = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
 const uint16_t PROGMEM qwet_combo[] = {KC_Q, KC_W, KC_E, KC_T, COMBO_END};
 const uint16_t PROGMEM yiop_combo[] = {KC_Y, KC_I, KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM qet_combo[] = {KC_Q, KC_E, KC_T, COMBO_END};
 const uint16_t PROGMEM yip_combo[] = {KC_Y, KC_I, KC_P, COMBO_END};
+const uint16_t PROGMEM tg_combo[] = {KC_T, LALT_T(KC_G), COMBO_END};
 
 
 combo_t key_combos[] = {
@@ -47,14 +49,15 @@ combo_t key_combos[] = {
     [QW_TAB_] = COMBO(qw_combo, KC_TAB),
     [WS_TAB_] = COMBO(ws_combo, KC_TAB),
     [PUNC_TAB_] = COMBO(punc_combo, KC_TAB),
+    [DSLH_TERM] = COMBO(dslh_combo, LCTL(KC_GRV)),
     [ZX_GRV_] = COMBO(zx_combo, KC_GRV),
-    [ZA_GRV_] = COMBO(zz_combo, KC_GRV),
     [CV_UNDS] = COMBO(cv_combo, KC_UNDS),
     [FJ_OSS_] = COMBO(fj_combo, OSM(MOD_LSFT)),
     [QWET_BOOT] = COMBO(qwet_combo, QK_BOOT),
     [YIOP_BOOT] = COMBO(yiop_combo, QK_BOOT),
-    [QET_MO3_] = COMBO(qet_combo, OSL(3)),
-    [YIP_MO3_] = COMBO(yip_combo, OSL(3)),
+    [QET_OSL3] = COMBO(qet_combo, OSL(3)),
+    [YIP_OSL3] = COMBO(yip_combo, OSL(3)),
+    [TG_WBAK] = COMBO(tg_combo, KC_WBAK),
 };
 
 #ifdef COMMUNITY_MODULE_CUSTOM_SHIFT_KEYS_ENABLE
