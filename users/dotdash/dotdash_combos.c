e64f8f92ddbf4adb79e7b00352b93bd5c39e6929
Josh DeWitt <josh.dewitt@skydio.com>
2026-08-14 13:51:17 -0700

    chore: consolidate overrides & key_combos to user module
    make it easier to use the same layout on several boards

---
+++ users/dotdash/dotdash_combos.c
@@ -0,0 +1,76 @@
+#include QMK_KEYBOARD_H
+
+enum combos {
+    DK_TG2_,
+    L2_TG2_,
+    QW_TAB_,
+    WS_TAB_,
+    DSLH_ENT_,
+    PUNC_TAB_,
+    ZX_TERM_,
+    CV_UNDS,
+    UJ_LPRN,
+    IK_RPRN,
+    OL_MINS,
+    FJ_OSS_,
+    QWET_BOOT,
+    YIOP_BOOT,
+    QET_OSL3,
+    YIP_OSL3,
+    TG_WBAK,
+    PQOT_COLN,
+    ED_COLN,
+};
+
+const uint16_t PROGMEM uj_combo[] = {KC_U, RSFT_T(KC_J), COMBO_END};
+const uint16_t PROGMEM ik_combo[] = {KC_I, LT(2,KC_K), COMBO_END};
+const uint16_t PROGMEM ol_combo[] = {KC_O, RCTL_T(KC_L), COMBO_END};
+const uint16_t PROGMEM dk_combo[] = {LT(2,KC_D), LT(2,KC_K), COMBO_END};
+const uint16_t PROGMEM L2_combo[] = {LT(2,KC_3), LT(2,KC_8), COMBO_END};  // return from L2
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM ws_combo[] = {KC_W, RCTL_T(KC_S), COMBO_END};
+const uint16_t PROGMEM punc_combo[] = {LALT_T(KC_COMM), LCTL_T(KC_DOT), COMBO_END};
+const uint16_t PROGMEM dslh_combo[] = {LCTL_T(KC_DOT), RSFT_T(KC_SLSH), COMBO_END};
+const uint16_t PROGMEM zx_combo[] = {LSFT_T(KC_Z), LCTL_T(KC_X), COMBO_END};
+const uint16_t PROGMEM cv_combo[] = {LALT_T(KC_C), LGUI_T(KC_V), COMBO_END};
+const uint16_t PROGMEM fj_combo[] = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
+const uint16_t PROGMEM qwet_combo[] = {KC_Q, KC_W, KC_E, KC_T, COMBO_END};
+const uint16_t PROGMEM yiop_combo[] = {KC_Y, KC_I, KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM qet_combo[] = {KC_Q, KC_E, KC_T, COMBO_END};
+const uint16_t PROGMEM yip_combo[] = {KC_Y, KC_I, KC_P, COMBO_END};
+const uint16_t PROGMEM tg_combo[] = {KC_T, LALT_T(KC_G), COMBO_END};
+const uint16_t PROGMEM pqot_combo[] = {KC_P, RALT_T(KC_QUOT), COMBO_END};
+const uint16_t PROGMEM ed_combo[] = {KC_E, LT(2, KC_D), COMBO_END};
+
+
+combo_t key_combos[] = {
+    [UJ_LPRN] = COMBO(uj_combo, KC_LPRN),
+    [IK_RPRN] = COMBO(ik_combo, KC_RPRN),
+    [OL_MINS] = COMBO(ol_combo, KC_MINS),
+    [DK_TG2_] = COMBO(dk_combo, TG(2)),
+    [L2_TG2_] = COMBO(dk_combo, TG(2)),
+    [QW_TAB_] = COMBO(qw_combo, KC_TAB),
+    [WS_TAB_] = COMBO(ws_combo, KC_TAB),
+    [DSLH_ENT_] = COMBO(dslh_combo, KC_ENT),
+    [PUNC_TAB_] = COMBO(punc_combo, KC_TAB),
+    [ZX_TERM_] = COMBO(zx_combo, LCTL(KC_GRV)),
+    [CV_UNDS] = COMBO(cv_combo, KC_UNDS),
+    [FJ_OSS_] = COMBO(fj_combo, OSM(MOD_LSFT)),
+    [QWET_BOOT] = COMBO(qwet_combo, QK_BOOT),
+    [YIOP_BOOT] = COMBO(yiop_combo, QK_BOOT),
+    [QET_OSL3] = COMBO(qet_combo, OSL(3)),
+    [YIP_OSL3] = COMBO(yip_combo, OSL(3)),
+    [TG_WBAK] = COMBO(tg_combo, KC_WBAK),
+    [PQOT_COLN] = COMBO(pqot_combo, KC_COLN),
+    [ED_COLN] = COMBO(ed_combo, KC_COLN),
+};
+
+#ifdef COMMUNITY_MODULE_CUSTOM_SHIFT_KEYS_ENABLE
+#include "modules/getreuer/custom_shift_keys/custom_shift_keys.h"
+const custom_shift_key_t custom_shift_keys[] = {
+    {LT(1,KC_SPC), KC_UNDS},  // Shift Space is _
+    {KC_SPC, KC_UNDS},         // Shift Space is _
+    {RALT_T(KC_0), KC_COLN},   // Shift 0 → :
+    {KC_COLN, KC_SCLN},        // Shift P+' combo → ;
+};
+#endif
---
+++ users/dotdash/dotdash_overrides.c
@@ -0,0 +1,186 @@
+#include QMK_KEYBOARD_H
+#include "print.h"
+
+/* Note on defined keys:
+ ** QK_USER_0: paste code block (``` paste ```)
+ ** QK_USER_1: Layer word for numbers (L2)
+ ** QK_USER_2: layer word for numpad (L4)
+ ** QK_USER_3: layer word for numpad (L4) && Swap hands (cad num entry)
+ ** QK_USER_4: nav layer word?? (todo)
+*/
+
+bool remember_last_key_user(uint16_t keycode, keyrecord_t *record, uint8_t *remembered_mods) {
+    switch (keycode) {
+        case LALT_T(KC_R):
+            return false;
+    }
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(4, KC_S):
+            if (record->tap.count && record->event.pressed) {
+                set_oneshot_mods(MOD_BIT(KC_LSFT));
+                return false;
+            }
+            return true;
+        case QK_USER_0:
+        case LCTL_T(QK_USER_0):
+            if (record->event.pressed) {
+                SEND_STRING(SS_LSFT(SS_TAP(X_ENT)) SS_DELAY(100));
+                SEND_STRING("```" SS_LSFT(SS_TAP(X_ENT)) SS_DELAY(100));
+                tap_code(KC_PSTE);
+                SEND_STRING(SS_DELAY(50) SS_LSFT(SS_TAP(X_ENT)) "```");
+            }
+            return false;
+        default:
+            break;
+    }
+
+    if (debug_enable) {
+        switch (keycode) {
+            case LSFT_T(KC_F):
+            case RSFT_T(KC_J):
+            case LSFT_T(KC_Z):
+            case RSFT_T(KC_SLSH):
+                if (record->event.pressed) {
+                    dprintf("SFT_DN:0x%04X t=%u\n", keycode, record->event.time);
+                } else {
+                    dprintf("SFT_UP:0x%04X t=%u tap=%u\n", keycode, record->event.time,
+                            record->tap.count);
+                }
+                break;
+            case KC_BSPC:
+                if (record->event.pressed) {
+                    dprintf("BSPC t=%u\n", record->event.time);
+                }
+                break;
+        }
+    }
+    return true;
+}
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));
+            return true;
+        case KC_MINS: // don't turn into underscore
+        case KC_1 ... KC_0:
+        case KC_UNDS:
+        case KC_BSPC:
+        case KC_LSFT:
+        case KC_RSFT:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool is_flow_tap_key(uint16_t keycode) {
+    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
+        return false; // Disable Flow Tap on hotkeys.
+    }
+    switch (get_tap_keycode(keycode)) {
+        case KC_A ... KC_Z:
+        case KC_DOT:
+        case KC_COMM:
+        case KC_SCLN:
+        case KC_SLSH:
+            return true;
+    }
+    return false;
+}
+
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
+    if (prev_keycode == KC_BSPC) {
+        return 0;
+    }
+
+    bool is_shift_mt = false;
+    uint16_t term;
+    switch (keycode) {
+        case LSFT_T(KC_F):
+        case RSFT_T(KC_J):
+            is_shift_mt = true;
+            term = FLOW_TAP_TERM_FAST;
+            break;
+        case LT(1, KC_SPC):
+            term = FLOW_TAP_TERM_FAST;
+            break;
+        default:
+            term = FLOW_TAP_TERM;
+            break;
+    }
+
+    if (is_shift_mt && prev_keycode == LT(1, KC_SPC)) {
+        return 0;
+    }
+    return term;
+}
+
+
+// Layer Word callbacks
+uint8_t get_layerword_layer_from_trigger(uint16_t keycode) {
+    switch (keycode) {
+        case QK_USER_1:
+            return 2;
+        case QK_USER_2:
+            return 4;
+        case QK_USER_3:  // numpad layer & swap hands
+        case LSFT_T(QK_USER_3):
+            dprintf("numpad SWAP hands ON");
+            swap_hands_on();
+            return 4;
+        default:
+            return 0;
+    }
+}
+
+bool should_continue_layerword(uint8_t layer, uint16_t keycode, keyrecord_t *record) {
+    switch (layer) {
+        case 2:
+            switch (keycode) {
+                case KC_1 ... KC_0:
+                case KC_MINS:
+                case KC_EQL:
+                case KC_BSLS:
+                case KC_GRV:
+                case KC_DOT:
+                case KC_COMM:
+                case KC_SLSH:
+                case KC_BSPC:
+                case KC_DEL:
+                    return true;
+                default:
+                    return false;
+            }
+        case 4:
+            switch(keycode) {
+                case KC_A ... KC_F: // support hex entry
+                case KC_1 ... KC_0:
+                case KC_MINS:
+                case KC_EQL:
+                case KC_BSLS:
+                case KC_GRV:
+                case KC_DOT:
+                case KC_COMM:
+                case KC_SLSH:
+                case KC_BSPC:
+                case KC_DEL:
+                case KC_TAB: // spreadsheet mode
+                return true;
+                default:
+                    dprintf("numpad SWAP hands OFF, %d / %s", keycode, keycode);
+                    swap_hands_off();
+                    return false;
+            }
+        }
+    return false;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, 1, 4, 5);
+    return state;
+}
