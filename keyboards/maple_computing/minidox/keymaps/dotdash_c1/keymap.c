337d8670e661387e1d3b83f7fef534e486b1aadf
Josh DeWitt <josh.dewitt@skydio.com>
2026-08-19 19:23:48 -0700

    feat/chore: use shared minidox keymap
    fixes some build issues, should be much easeier to share values now.
    
    next step, building for cameldox??

--- keyboards/maple_computing/minidox/keymaps/dotdash_c1/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/dotdash_c1/keymap.c
@@ -1,12 +1,47 @@
 #include QMK_KEYBOARD_H
-#include "users/dotdash/dotdash_layers.h"
+#include "users/dotdash/dotdash.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE]   = LAYOUT_split_3x5_3(DD_LAYER_0),
-    [_NAV]    = LAYOUT_split_3x5_3(DD_LAYER_1),
-    [_NUM]    = LAYOUT_split_3x5_3(DD_LAYER_2),
-    [_SYS]    = LAYOUT_split_3x5_3(DD_LAYER_3),
-    [_NUMPAD] = LAYOUT_split_3x5_3(DD_LAYER_4),
-    [_MOUSE]  = LAYOUT_split_3x5_3(DD_LAYER_5),
-    [_FN]     = LAYOUT_split_3x5_3(DD_LAYER_6),
+    [_BASE] = DD_LAYOUT(
+        DD_L0_TOP_L, DD_L0_TOP_R,
+        DD_L0_HRM_L, DD_L0_HRM_R,
+        DD_L0_BOT_L, DD_L0_BOT_R,
+        DD_L0_THM_L, DD_L0_THM_R
+    ),
+    [_NAV] = DD_LAYOUT(
+        DD_L1_TOP_L, DD_L1_TOP_R,
+        DD_L1_HRM_L, DD_L1_HRM_R,
+        DD_L1_BOT_L, DD_L1_BOT_R,
+        DD_L1_THM_L, DD_L1_THM_R
+    ),
+    [_NUM] = DD_LAYOUT(
+        DD_L2_TOP_L, DD_L2_TOP_R,
+        DD_L2_HRM_L, DD_L2_HRM_R,
+        DD_L2_BOT_L, DD_L2_BOT_R,
+        DD_L2_THM_L, DD_L2_THM_R
+    ),
+    [_SYS] = DD_LAYOUT(
+        DD_L3_TOP_L, DD_L3_TOP_R,
+        DD_L3_HRM_L, DD_L3_HRM_R,
+        DD_L3_BOT_L, DD_L3_BOT_R,
+        DD_L3_THM_L, DD_L3_THM_R
+    ),
+    [_NUMPAD] = DD_LAYOUT(
+        DD_L4_TOP_L, DD_L4_TOP_R,
+        DD_L4_HRM_L, DD_L4_HRM_R,
+        DD_L4_BOT_L, DD_L4_BOT_R,
+        DD_L4_THM_L, DD_L4_THM_R
+    ),
+    [_MOUSE] = DD_LAYOUT(
+        DD_L5_TOP_L, DD_L5_TOP_R,
+        DD_L5_HRM_L, DD_L5_HRM_R,
+        DD_L5_BOT_L, DD_L5_BOT_R,
+        DD_L5_THM_L, DD_L5_THM_R
+    ),
+    [_FN] = DD_LAYOUT(
+        DD_L6_TOP_L, DD_L6_TOP_R,
+        DD_L6_HRM_L, DD_L6_HRM_R,
+        DD_L6_BOT_L, DD_L6_BOT_R,
+        DD_L6_THM_L, DD_L6_THM_R
+    ),
 };
